3ce7adc17cdb58a90b2e48838eda244da6a3b72c
dhk767 <dhk767@gmail.com>
2022-11-24 10:57:00 +0900

    Initial Commit

---
+++ BlackHistory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackHistory
+//
+//  Created by 김동현 on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
